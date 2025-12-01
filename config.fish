ecbe120b67f454e29e66dc84a08631aa7cb1a6b1
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2025-12-01 22:20:26 +0530

    ding dong

---
+++ config.fish
@@ -0,0 +1,29 @@
+# Git aliases
+alias gs "git status"
+alias ga "git add"
+alias gc "git commit -m"
+alias gp "git push"
+alias gl "git pull"
+alias gb "git branch"
+alias gd "git diff"
+alias gco "git checkout"
+alias gcb "git checkout -b"
+alias y yazi
+alias cr "cargo run"
+
+alias ca cursor-agent
+
+alias c cursor
+
+starship init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+fish_add_path $HOME/.local/bin
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+fish_add_path /Users/shivam/.spicetify
