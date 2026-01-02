915b73c1d2d04efcf277930f2be4dde1cb887c2f
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2026-01-02 08:06:42 +0530

    i might buy claude

--- config.fish
+++ config.fish
@@ -8,48 +8,16 @@ alias gb "git branch"
 alias gd "git diff"
 alias gco "git checkout"
 alias gcb "git checkout -b"
-alias y "yazi"
+alias y yazi
 alias cr "cargo run"
-alias oc "opencode"
-alias z "zed"
-alias cc "claude"
-alias ca "cursor-agent"
-alias c "cursor"
+alias oc opencode
+alias z zed
+alias cc claude
+alias ca cursor-agent
+alias c cursor
 alias tls "tmux list-sessions"
 
 starship init fish | source
-
-# this is for the the claude code openrouter integration.
-set -l env_file ~/.config/fish/.env
-
-if test -f "$env_file"
-    for line in (cat "$env_file")
-        # skip empties and comments
-        test -z "$line";   and continue
-        string match -q '#*' "$line"; and continue
-
-        # split only on the FIRST '='
-        set -l key   (string split -m 1 '=' "$line")[1]
-        set -l value (string split -m 1 '=' "$line")[2]
-
-        # export it to the environment
-        set -gx $key $value
-    end
-else
-    echo "env file not found: $env_file" >&2
-end
-
-
-export ANTHROPIC_BASE_URL="https://openrouter.ai/api"
-export ANTHROPIC_AUTH_TOKEN="$OPENROUTER_CLAUDE_INTEGRATION_KEY"
-export ANTHROPIC_API_KEY="" # Important: Must be explicitly empty
-
-export ANTHROPIC_DEFAULT_SONNET_MODEL="kwaipilot/kat-coder-pro:free"
-export ANTHROPIC_DEFAULT_OPUS_MODEL="kwaipilot/kat-coder-pro:free"
-export ANTHROPIC_DEFAULT_HAIKU_MODEL="kwaipilot/kat-coder-pro:free"
-
-
-
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
